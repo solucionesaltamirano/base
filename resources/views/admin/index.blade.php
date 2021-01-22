91a5d5049623c013351d8283535bd3e79de5e0d2
gersonaltamirano <gersonaltamirano@gmail.com>
2021-01-22 17:41:24 -0600

    Paquete AdminLTE3

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin')
+
+@section('content_header')
+    <h1>Administración</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido a su area de administración</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
