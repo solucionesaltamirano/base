c30016f1f2f8a1882e5c2eb05887d1653d695228
gersonaltamirano <gersonaltamirano@gmail.com>
2021-01-22 12:54:09 -0600

    vista para hacer pruebas

--- resources/views/livewire/prueba.blade.php
+++ resources/views/livewire/prueba.blade.php
@@ -10,9 +10,21 @@
     @if ( $notification )
         <h1>Tiene notificaciones</h1>            
     @else
-        <h1>        <i class="fad fa-address-book"></i> NO TIENE notificaciones</h1>            
+        <h1> <i class="fad fa-address-book"></i> NO TIENE notificaciones</h1>            
         
     @endif
+
+    @foreach ($icons as $icon) 
+
+    <div class="inline-block bg-gray-100 border border-indigo-700" >
+        
+        <span class='fa-3x text-blue-700 {{ $icon->categoryfaicon->class }}  ' > {!! $icon->value !!}  </span>
+    </div>
+   
+
+     @endforeach
+
+   
 
 </div>
 
